e2c6025e1c3699c0e644387249e341386680a8cd
jeffer <jeffe.bernal@gmail.com>
2018-09-21 12:30:21 -0500

    views using pug

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
 const express = require('express')
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
 app.get('/', (req, res) =>{
-  var i = 0;
-  var message = ''
-  do {
-    i += 1;
-    if(i % 2 == 0)
-      message += '<p>' + i + ' Soy Par!</p>'
-    else
-      message += '<p>' + i + ' Soy Impar!</p>'
-  } while (i < 50);
-  res.send(message);
+  var messages = []
+  for (var i = 1; i <= 50; i++){
+    messages.push(i + ' Soy ' + (i % 2 == 0 ? 'Par!' : 'Impar!'))
+  }
+  res.render('index', { messages: messages });
 });
 
 app.listen(3000, () => console.log('listening 3000'));
